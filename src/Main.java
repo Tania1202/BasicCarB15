37846273b7aff3a4e78d5e63b7f916683c94e4c2
Tania1202 <lta120292@gmail.com>
2023-03-30 14:19:04 -0400

    the lights have been added

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,10 @@ public class Main {
         X.cylinders = "8";
         X.size = "1234cc";
         X.weight = "645k";
+
+        lights neon=new lights();
+        neon.color="red";
+        neon.size="15cm";
+        neon.intensity="123434limit";
     }
 }
